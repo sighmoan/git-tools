95733f33d29d82242aaf39e47a65fe9d10b1fc7d
Simon Kendall <simon.kendall@ica.se>
2025-02-25 12:56:43 +0100

    add git config commands list

---
+++ gitconfig.sh
@@ -0,0 +1,15 @@
+git config --global commit.verbose true
+git config --global push.autoSetupRemote true
+git config --global push.default simple
+
+git config --global diff.colorMoved plain
+git config --global diff.mnemonicPrefix true
+git config --global diff.renames true
+
+git config --global diff.algorithm histogram
+
+git config --global branch.sort -committerdate
+
+git config --global help.autocorrect prompt
+
+
