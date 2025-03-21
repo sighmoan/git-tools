3871b3ed6ac137ea74eeb7a150231081ef78544a
Simon Kendall <simon.kendall@ica.se>
2025-03-21 12:53:05 +0100

    let jira . open the current ticket

--- script/navigate-to-jira-ticket.sh
+++ script/navigate-to-jira-ticket.sh
@@ -3,8 +3,10 @@
 input=$1
 
 if [[ $input =~ ^[0-9]+$ ]] && [[ ${#input} -le 5 ]]; then
-    ticket=${1:-`git branch --show-current 2> /dev/null | sed -E "s/"$JIRA_TICKET_PREFIX"([0-9]+).*/\1/"`}
-else
+    ticket=$input
+elif [[ $input == "." ]]; then
+    ticket=$(git branch --show-current 2> /dev/null | sed -E "s/"$JIRA_TICKET_PREFIX"([0-9]+).*/\1/")
+elif [[ $input ]]; then
     ticket=$(git show $input | grep -oE "$JIRA_TICKET_PREFIX[0-9]+" | sed "s/$JIRA_TICKET_PREFIX//g")
 
     if [ -z "$ticket" ]; then
@@ -17,4 +19,4 @@ if [ "$ticket" ]
     then 
         git web--browse $JIRA_URL"/browse/"$JIRA_TICKET_PREFIX$ticket && exit
 fi
-git web--browse $JIRA_BOARD_URL"?assignee="$JIRA_ID
+git web--browse $JIRA_BOARD_URL"?assignee="$JIRA_USER_ID
